3f1b7763bee2b5b3892bb85c5fc59b439d3b886d
Leandro <leandro.roberto.medeiros@everis.com>
2019-04-12 15:18:03 -0300

    IMC Calculator iOS

---
+++ IMC/IMC/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  IMC
+//
+//  Created by Leandro Roberto Medeiros on 06/04/19.
+//  Copyright © 2019 Leandro Roberto Medeiros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfWeight: UITextField!
+    @IBOutlet weak var tfHeight: UITextField!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var ivResult: UIImageView!
+    @IBOutlet weak var viResults: UIView!
+   
+    var imc : Double = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+
+    @IBAction func calculate(_ sender: Any) {
+        
+        if let weight = Double(tfWeight.text!), let height = Double(tfHeight.text!){
+            imc = weight / (height * height)
+            showResults()
+        }
+        
+    }
+    
+    func showResults(){
+        var result: String = ""
+        var image: String = ""
+            switch imc {
+            case 0..<16:
+                result = "Magreza"
+                image = "magreza"
+            case 16..<18.5:
+                result = "Abaixo do Peso"
+                image = "abaixo"
+            case 18.5..<25:
+                result = "Peso Ideal"
+                image = "ideal"
+            case 25..<30:
+                result = "SobrePeso"
+                image = "sobre"
+            default:
+                result = "Obesidade"
+                image = "obesidade"
+        }
+        
+        lbResult.text = "\(Int(imc)): \(result)"
+        ivResult.image = UIImage(named: image)
+        viResults.isHidden = false
+        view.endEditing(true)
+    }
+    
+}
+
